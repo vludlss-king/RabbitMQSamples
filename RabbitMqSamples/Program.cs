41f1377b21e973fe77b22065a865e1289d3ca6ab
vludlss <vladislav.mikkoev@yandex.ru>
2024-02-21 20:26:41 +0300

    feat: Добавить отсылку, получение сообщения

--- RabbitMqSamples/Program.cs
+++ RabbitMqSamples/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace RabbitMqSamples
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitMqSamples
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var factory = new ConnectionFactory { HostName = "localhost", VirtualHost = "/", UserName = "guest", Password = "guest" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "hello",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            const string message = "Hello World!";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            Console.WriteLine($"[x] sent {message}");
+            channel.BasicPublish(exchange: string.Empty,
+                routingKey: "hello",
+                basicProperties: null,
+                body: body);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, args) =>
+            {
+                var body = args.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"[x] Received {message}");
+            };
+
+            channel.BasicConsume(queue: "hello",
+                autoAck: true,
+                consumer: consumer);
+
+            Console.WriteLine("Press [enter] to exit.");
+            Console.ReadLine();
         }
     }
 }
